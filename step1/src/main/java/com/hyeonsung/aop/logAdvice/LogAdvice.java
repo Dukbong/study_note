196da764eb1502cb3c0ca52bddd0bda6c54385fa
jang <gorae930301@gmail.com>
2023-08-01 02:29:58 +0900

    Implement AOP using Spring

---
+++ step1/src/main/java/com/hyeonsung/aop/logAdvice/LogAdvice.java
@@ -0,0 +1,19 @@
+package com.hyeonsung.aop.logAdvice;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class LogAdvice implements MethodInterceptor{
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		long start = System.currentTimeMillis();
+		Object result = invocation.proceed();
+		// 이건 methos.invoke(class,args)와 같은 것이다.
+		long end = System.currentTimeMillis();
+		String message = (end - start) + "ms 걸렸습니다.";
+		System.out.println(message);
+		return result;
+	}
+
+}
